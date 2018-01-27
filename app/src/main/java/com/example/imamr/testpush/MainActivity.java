be4738a6d91dc74c35f91c76c5fffc4e37024897
Imam R <iimrobani@gmail.com>
2018-01-27 23:17:28 +0700

    Initial commit

---
+++ app/src/main/java/com/example/imamr/testpush/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.imamr.testpush;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void test(){
+        int i;
+
+        i = 1 ;
+    }
+}
